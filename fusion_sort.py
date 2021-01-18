3e560afd4c9b0e3118b496a573016ded17097781
Hugo Pérard <hugo.perard@bearstudio.fr>
2021-01-18 14:50:53 +0100

    remove iteration from fusion_sort

--- fusion_sort.py
+++ fusion_sort.py
@@ -1,12 +1,9 @@
 import sys
 import timeit
 
-comparison = 0
-iteration = 0
-
 
 def fusion(tab1, tab2):
-    global comparison
+    comparison = 0
     l1 = len(tab1)
     l2 = len(tab2)
     complete_tab = [0] * (l1 + l2)
@@ -39,36 +36,36 @@ def fusion(tab1, tab2):
     return complete_tab
 
 
-def tri_fusion_recursif(tab_data):
-    global comparison, iteration
-    iteration = iteration + 1
+def tri_fusion_recursif(tab_data, comparison):
     n = len(tab_data)
     comparison = comparison + 1
+    recusive_result1 = [0, 0]
+    recusive_result2 = [0, 0]
     if n > 1:
         p = int(n/2)
         tab1 = tab_data[0:p]
         tab2 = tab_data[p:n]
-        tri_fusion_recursif(tab1)
-        tri_fusion_recursif(tab2)
+        recusive_result1 = tri_fusion_recursif(tab1, comparison)
+        recusive_result2 = tri_fusion_recursif(tab2, comparison)
         tab_data[:] = fusion(tab1, tab2)
 
-    return tab_data
+    return tab_data, recusive_result1[1] + recusive_result2[1] + comparison
 
 
 def fusion_sort(data):
+    comparison = 0
     tab_data = data.split(";")
 
-    tri_fusion_recursif(tab_data)
+    recusive_result = tri_fusion_recursif(tab_data, comparison)
 
-    return tab_data, comparison, iteration
+    return tab_data, recusive_result[1]
 
 
 result = fusion_sort(sys.argv[1])
 
 print("Serie : " + sys.argv[1])
 print("Resultat : " + ";".join(result[0]))
-print("Nb de comparaison : " + str(comparison))
-print("Nb d'iteration : " + str(iteration))
+print("Nb de comparaison : " + str(result[1]))
 
 time = timeit.Timer(lambda: fusion_sort(sys.argv[1]))
 print("Temps (sec) : " + str(time.timeit(10000) / 10000))
